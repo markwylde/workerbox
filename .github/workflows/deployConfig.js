e636da5f2256af99bac228cd01c20b82b24bf1af
Mark Wylde <me@markwylde.com>
2022-11-20 19:49:29 +0000

    Fix deploy script

--- .github/workflows/deployConfig.js
+++ .github/workflows/deployConfig.js
@@ -1,15 +1,18 @@
 import info from '../../package.json' assert { type: 'json' };
 
-export default [{
-  hostname: process.env.SUB_FTP_HOSTNAME,
-  username: process.env.SUB_FTP_USERNAME,
-  password: process.env.SUB_FTP_PASSWORD,
-  source: './server/dist',
-  destination: 'v' + info.version
-}, {
-  hostname: process.env.TOP_FTP_HOSTNAME,
-  username: process.env.TOP_FTP_USERNAME,
-  password: process.env.TOP_FTP_PASSWORD,
-  source: './demo',
-  destination: ''
-}];
+export default {
+  tasks: [{
+    hostname: process.env.SUB_FTP_HOSTNAME,
+    username: process.env.SUB_FTP_USERNAME,
+    password: process.env.SUB_FTP_PASSWORD,
+    source: './server/dist',
+    destination: 'v' + info.version
+  }, {
+    hostname: process.env.TOP_FTP_HOSTNAME,
+    username: process.env.TOP_FTP_USERNAME,
+    password: process.env.TOP_FTP_PASSWORD,
+    source: './demo',
+    destination: '',
+    clearDestination: true
+  }]
+}
