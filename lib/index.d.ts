da5c30cc908ba2b3ed818575a3033ca922ec9154
Tomer Aberbach <tomer@aberba.ch>
2024-11-14 21:11:07 -0500

    feat: improve types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,9 +1,17 @@
 declare type WorkerBoxOptions = {
   appendVersion?: boolean;
 };
+
 declare type Scope = object;
-declare function createWorkerBox(scriptUrl: any, options?: WorkerBoxOptions): Promise<{
+
+export type WorkerBox = {
   run: (code: string, scope?: Scope) => Promise<any>;
-  destroy: () => any;
-}>
+  destroy: () => void;
+};
+
+declare function createWorkerBox(
+  scriptUrl?: string,
+  options?: WorkerBoxOptions
+): Promise<WorkerBox>;
+
 export default createWorkerBox;
